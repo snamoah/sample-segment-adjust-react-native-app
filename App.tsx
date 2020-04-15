b863f2fff50ebcbdfaaec4c863a1662298078fc4
Samuel Amoah <samuel@bitwala.com>
2020-04-15 12:28:35 +0200

    add segment to app

--- App.tsx
+++ App.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import analytics from '@segment/analytics-react-native';
+import Adjust from '@segment/analytics-react-native-adjust';
 import { StyleSheet, Text, View } from 'react-native';
 
+const WRITE_KEY = 'u58izp2B9ZmPwJXTBI7CHJ49aySV65XF';
+
+export const loadSegment = async () => {
+  await analytics.setup(WRITE_KEY, {
+    using: [Adjust],
+    /* 
+    These two should alway be false, otherwise segment 
+    in background will trigger some events automatically:
+    https://segment.zendesk.com/hc/en-us/requests/361126?page=1
+    */
+    flushAt: 1,
+    trackAttributionData: true,
+    recordScreenViews: false,
+    trackAppLifecycleEvents: true,
+  });
+};
+
 export default function App() {
+
+  useEffect(() => {
+    loadSegment();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
